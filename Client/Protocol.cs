41aa2ab80357855142e2f083c45679d199072fa3
comwrg <xcomwrg@gmail.com>
2017-10-11 12:52:02 +0800

    add class `Protocol` in project `Client`

---
+++ Client/Protocol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Protocol
+    {
+        private TcpClient _tcpClient;
+        private Socket _client;
+        public Protocol(string ip, int port)
+        {
+            _tcpClient = new TcpClient();
+            _tcpClient.Connect(ip, port);
+            _client = _tcpClient.Client;
+
+
+        }
+
+        public void Send(string msg)
+        {
+            _client.Send(Encoding.UTF8.GetBytes(msg));
+        }
+    }
+}
